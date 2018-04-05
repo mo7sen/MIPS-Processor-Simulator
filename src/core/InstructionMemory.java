0b83193298ebd56e1328049b84a7bd830edcb0ed
MO7SEN <31735762+mo7sener@users.noreply.github.com>
2018-04-05 21:25:48 +0200

    Added reset method to InstructionMemory

--- src/core/InstructionMemory.java
+++ src/core/InstructionMemory.java
@@ -5,12 +5,16 @@ import java.util.ArrayList;
 public class InstructionMemory
 {
 	private static ArrayList<String> inMem = new ArrayList<>();
-	public void add(String ins)
+	public static void add(String ins)
 	{
 		inMem.add(ins);
 	}
-	public String read(int address)
+	public static String read(int address)
 	{
 		return inMem.get(address/4);
+	}
+	public static void reset()
+	{
+		inMem.clear();
 	}
 }
