ca8c969dd7bbfca2ab59eae58b333d0ebf2e01ac
Mohamedalaa98 <32443456+Mohamedalaa98@users.noreply.github.com>
2018-04-18 02:53:02 -0700

    Update Assembler.java

--- src/core/Assembler.java
+++ src/core/Assembler.java
@@ -8,7 +8,7 @@ public class Assembler
 	static ArrayList<Label> labels = new ArrayList<>();
 	static ArrayList<String> codeLines;
 
-	private static Label findLabel(String name)
+	private static Label findLabel(String name) // finds the address of a label
 	{
 		for(Label lbl: labels)
 		{
@@ -20,7 +20,7 @@ public class Assembler
 
 	static void assembleProgram(String s)
 	{
-		codeLines = new ArrayList<>(Arrays.asList(s.replaceAll(":", ":\n").split("\\n+")));
+		codeLines = new ArrayList<>(Arrays.asList(s.replaceAll(":", ":\n").split("\\n+")));	//Labels now take a whole line for themselves
 		labels.clear();
 		scanForLabels();
 		assembleLines();
@@ -57,37 +57,37 @@ public class Assembler
 				syn = syn.replaceAll("f+|o+", instruction.opc);
 				switch (instruction.syn)
 				{
-					case ArithLog:
-					case JumpR:
-					case DivMult:
-					case ShiftV:
-					case MoveFrom:
-					case MoveTo:
-					case Shift:
+					case ArithLog: //Register
+					case JumpR:	//Register
+					case DivMult:	//Register
+					case ShiftV:	//Register
+					case MoveFrom:	//Register
+					case MoveTo:	//Register
+					case Shift:	//Register
 						s = (instruction.syn == Syntax.JumpR || instruction.syn == Syntax.DivMult || instruction.syn == Syntax.MoveTo) ? Registers.findRegister(ss[1]).address : (instruction.syn == Syntax.ShiftV) ? Registers.findRegister(ss[3]).address : (instruction.syn != Syntax.MoveFrom && instruction.syn != Syntax.Shift) ? Registers.findRegister(ss[2]).address : "00000";
 						t = (instruction.syn == Syntax.JumpR || instruction.syn == Syntax.MoveFrom || instruction.syn == Syntax.MoveTo) ? "00000" : (instruction.syn == Syntax.DivMult || instruction.syn == Syntax.ShiftV || instruction.syn == Syntax.Shift) ? Registers.findRegister(ss[2]).address : Registers.findRegister(ss[3]).address;
 						d = (instruction.syn == Syntax.JumpR || instruction.syn == Syntax.DivMult || instruction.syn == Syntax.MoveTo) ? "00000" : Registers.findRegister(ss[1]).address;
 						a = (instruction.syn == Syntax.Shift) ? Integer.toBinaryString(Integer.parseInt(ss[3])) : "00000";
 						break;
-					case LoadI:
-					case ArithLogI:
+					case LoadI:	//Immediate
+					case ArithLogI:	//Immediate
 						t = Registers.findRegister(ss[1]).address;
 						s = (instruction.syn == Syntax.LoadI) ? "00000" : Registers.findRegister(ss[2]).address;
 						imm = (instruction.syn == Syntax.LoadI) ? Integer.toBinaryString(Integer.parseInt(ss[2])) : Integer.toBinaryString(Integer.parseInt(ss[3]));
 						break;
-					case LoadStore:
+					case LoadStore:	//Immediate
 						t = Registers.findRegister(ss[1]).address;
 						imm = Integer.toBinaryString(Integer.parseInt(ss[2].split("\\(")[0]));
 						s = Registers.findRegister(ss[2].split("\\(")[1].split("\\)")[0]).address;
 						break;
-					case BranchZ:
-					case Branch:
+					case BranchZ:	//Immediate
+					case Branch:	//Immediate
 						s = Registers.findRegister(ss[1]).address;
 						t = Registers.findRegister(ss[2]).address;
 						Label l = findLabel(ss[3].trim());
 						if (l != null)
 							imm = Integer.toBinaryString((Integer.parseInt(l.getAddress(), 2)/4) - (i + 1));
-					case Jump:
+					case Jump:	//Jump
 						Label l2 = findLabel(ss[1]);
 						if (l2 != null)
 							imm = l2.getAddress().substring(4, 30);
