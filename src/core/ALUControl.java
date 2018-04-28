4403ecf99d54a6d5ec9b06735b8026139a84e52a
MO7SEN <31735762+mo7sener@users.noreply.github.com>
2018-04-28 23:17:15 +0200

    Started the control units

--- src/core/ALUControl.java
+++ src/core/ALUControl.java
@@ -1,6 +1,94 @@
 package core;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class ALUControl
 {
-//	Data input, output;
+	static StringProperty controlIn = new SimpleStringProperty();
+	static StringProperty functIn = new SimpleStringProperty();
+	static StringProperty ALUOpOut = new SimpleStringProperty();
+	static StringProperty shift = new SimpleStringProperty();
+	static StringProperty jumpR = new SimpleStringProperty();
+	static StringProperty div = new SimpleStringProperty();
+	static StringProperty divMult = new SimpleStringProperty();
+	static StringProperty lo = new SimpleStringProperty();
+	static StringProperty mWrite = new SimpleStringProperty();
+	static StringProperty shiftDir = new SimpleStringProperty();
+	static StringProperty signed = new SimpleStringProperty();
+	static StringProperty aShift = new SimpleStringProperty();
+	static StringProperty mRead = new SimpleStringProperty();
+	static StringProperty regWrite = new SimpleStringProperty();
+
+	public static void execute()
+	{
+		switch(controlIn.get())
+		{
+			case "010":
+				executeRFormat();
+				break;
+			case "000"://add
+				shift.set("0");
+				jumpR.set("0");
+				divMult.set("0");
+				mWrite.set("0");
+				signed.set("0");
+				mRead.set("0");
+				regWrite.set("0");
+				break;
+			case "001"://sub
+				shift.set("0");
+				jumpR.set("0");
+				divMult.set("0");
+				mWrite.set("0");
+				signed.set("0");
+				mRead.set("0");
+				regWrite.set("0");
+				break;
+			case "011"://slt
+				shift.set("0");
+				jumpR.set("0");
+				divMult.set("0");
+				mWrite.set("0");
+				signed.set("0");
+				mRead.set("0");
+				regWrite.set("0");
+				break;
+			case "100"://and
+				shift.set("0");
+				jumpR.set("0");
+				divMult.set("0");
+				mWrite.set("0");
+				signed.set("0");
+				mRead.set("0");
+				regWrite.set("0");
+				break;
+			case "101"://or
+				shift.set("0");
+				jumpR.set("0");
+				divMult.set("0");
+				mWrite.set("0");
+				signed.set("0");
+				mRead.set("0");
+				regWrite.set("0");
+				break;
+			case "111"://xor
+				shift.set("0");
+				jumpR.set("0");
+				divMult.set("0");
+				mWrite.set("0");
+				signed.set("0");
+				mRead.set("0");
+				regWrite.set("0");
+				break;
+		}
+	}
+
+	private static void executeRFormat()
+	{
+		switch(functIn.get())
+		{
+
+		}
+	}
 }
---
+++ src/core/ControlUnit.java
@@ -0,0 +1,47 @@
+package core;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class ControlUnit
+{
+	static StringProperty opCodeIn = new SimpleStringProperty();
+	static StringProperty ALUOpOut = new SimpleStringProperty();
+	static StringProperty regDst = new SimpleStringProperty();
+	static StringProperty branch = new SimpleStringProperty();
+	static StringProperty memRead = new SimpleStringProperty();
+	static StringProperty memToReg = new SimpleStringProperty();
+	static StringProperty memWrite = new SimpleStringProperty();
+	static StringProperty ALUSrc = new SimpleStringProperty();
+	static StringProperty regWrite = new SimpleStringProperty();
+	static StringProperty byteOp = new SimpleStringProperty();
+	static StringProperty memToMem = new SimpleStringProperty();
+	static StringProperty signed = new SimpleStringProperty();
+	static StringProperty jump = new SimpleStringProperty();
+	static StringProperty link = new SimpleStringProperty();
+	static StringProperty equal = new SimpleStringProperty();
+	static StringProperty lui = new SimpleStringProperty();
+	static StringProperty halfOp = new SimpleStringProperty();
+
+	public static void execute()
+	{
+		switch (opCodeIn.get())
+		{
+			case "000000":
+				ALUOpOut.set("010");
+				regDst.set("1");
+				branch.set("0");
+				memToReg.set("0");
+				memWrite.set("0");
+				ALUSrc.set("0");
+				regWrite.set("0");
+				memToMem.set("0");
+				signed.set("0");
+				jump.set("0");
+				link.set("0");
+				equal.set("1");
+				lui.set("0");
+
+		}
+	}
+}
