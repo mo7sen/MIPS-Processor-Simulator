9df53155b88fc273362fd7971bbe0f523e67aa64
Mohamedalaa98 <32443456+Mohamedalaa98@users.noreply.github.com>
2018-04-29 19:12:14 -0700

    last

--- src/core/ALUControl.java
+++ src/core/ALUControl.java
@@ -87,8 +87,173 @@ public class ALUControl
 	private static void executeRFormat()
 	{
 		switch(functIn.get())
-		{
-
+		{ case "100000":
+			shift.set("1");
+			jumpR.set("0");
+			mWrite.set("0");
+			shiftDir.set("1");
+			signed.set("1");
+			mRead.set("0");
+			regWrite.set("1");
+			break;
+			case "10011":
+				shift.set("1");
+				jumpR.set("0");
+				mWrite.set("0");
+				shiftDir.set("1");
+				signed.set("1");
+				mRead.set("0");
+				regWrite.set("1");
+				break;
+			case "001000":
+				jumpR.set("1");
+				mWrite.set("0");
+				regWrite.set("0");
+				break;
+			case "101010":
+				shift.set("1");
+				jumpR.set("0");
+				mWrite.set("0");
+				shiftDir.set("1");
+				signed.set("1");
+				mRead.set("0");
+				regWrite.set("1");
+				break;
+			case "100010":
+			shift.set("1");
+			jumpR.set("0");
+			mWrite.set("0");
+			shiftDir.set("1");
+			signed.set("1");
+			mRead.set("0");
+			regWrite.set("1");
+				break;
+			case "011000":
+				jumpR.set("0");
+				div.set("0");
+				divMult.set("1");
+				mWrite.set("0");
+				signed.set("1");
+				regWrite.set("0");
+				break;
+			case "000010":
+				shift.set("1");
+				jumpR.set("0");
+				mWrite.set("0");
+				shiftDir.set("0");
+				signed.set("1");
+				aShift.set("0");
+				mRead.set("0");
+				regWrite.set("1");
+				break;
+			case "100100"	:
+				shift.set("1");
+				jumpR.set("0");
+				mWrite.set("0");
+				shift.set("1");
+				signed.set("1");
+				mRead.set("0");
+				regWrite.set("1");
+				break;
+			case "100101"	:
+				shift.set("1");
+				jumpR.set("0");
+				mWrite.set("0");
+				shift.set("1");
+				signed.set("1");
+				mRead.set("0");
+				regWrite.set("1");
+				break;
+			case "101001"	:
+				shift.set("1");
+				jumpR.set("0");
+				mWrite.set("0");
+				shift.set("1");
+				signed.set("0");
+				mRead.set("0");
+				regWrite.set("1");
+				break;
+			case "100001"	:
+				shift.set("1");
+				jumpR.set("0");
+				div.set("");
+				divMult.set("");
+				lo.set("");
+				mWrite.set("0");
+				shift.set("1");
+				signed.set("0");
+				aShift.set("");
+				mRead.set("0");
+				regWrite.set("1");
+				break;
+			case "011010"	:
+				jumpR.set("0");
+				div.set("1");
+				divMult.set("1");
+				mWrite.set("0");
+				signed.set("1");
+				regWrite.set("0");
+				break;
+			case "011011"	:
+				jumpR.set("0");
+				div.set("1");
+				divMult.set("1");
+				mWrite.set("0");
+				signed.set("0");
+				regWrite.set("0");
+				break;
+			case "011001"	:
+				jumpR.set("0");
+				div.set("0");
+				divMult.set("1");
+				mWrite.set("0");
+				signed.set("0");
+				regWrite.set("0");
+				break;
+			case "000011"	:
+				shift.set("1");
+				jumpR.set("0");
+				mWrite.set("0");
+				shift.set("0");
+				signed.set("1");
+				aShift.set("1");
+				mRead.set("0");
+				regWrite.set("1");
+				break;
+			case "100011"	:
+				shift.set("1");
+				jumpR.set("0");
+				mWrite.set("0");
+				shift.set("1");
+				signed.set("0");
+				mRead.set("0");
+				regWrite.set("1");
+				break;
+			case "100110"	:
+				shift.set("1");
+				jumpR.set("0");
+				mWrite.set("0");
+				shift.set("1");
+				signed.set("1");
+				mRead.set("0");
+				regWrite.set("1");
+				break;
+			case "010000"	:
+				jumpR.set("0");
+				divMult.set("");
+				lo.set("0");
+				mWrite.set("0");
+				mRead.set("1");
+				regWrite.set("0");
+				break;
+			case "010010"	:
+				jumpR.set("0");
+				lo.set("1");
+				mWrite.set("0");
+				mRead.set("1");
+				regWrite.set("0");
+				break;
 		}
+
 	}
 }
--- src/core/ControlUnit.java
+++ src/core/ControlUnit.java
@@ -42,6 +42,153 @@ public class ControlUnit
 				equal.set("1");
 				lui.set("0");
 
+			case "001000":
+				regDst.set("0");
+				branch.set("0");
+				memToReg.set("0");
+				memWrite.set("0");
+				ALUSrc.set("1");
+				regWrite.set("1");
+				memToMem.set("0");
+				signed.set("1");
+				ALUOpOut.set("000");
+				jump.set("0");
+				link.set("0");
+				equal.set("1");
+				lui.set("0");
+				break;
+
+			case "100011":
+				regDst.set("0");
+				branch.set("0");
+				memRead.set("1");
+				memToReg.set("1");
+				memWrite.set("0");
+				ALUSrc.set("1");
+				regWrite.set("1");
+				byteOp.set("0");
+				memToMem.set("0");
+				signed.set("1");
+				ALUOpOut.set("000");
+				jump.set("0");
+				link.set("0");
+				equal.set("1");
+				lui.set("0");
+				halfOp.set("0");
+				break;
+
+			case "101011":
+				branch.set("0");
+				memWrite.set("1");
+				ALUSrc.set("1");
+				regWrite.set("0");
+				memToMem.set("0");
+				signed.set("1");
+				ALUOpOut.set("000");
+				jump.set("0");
+				equal.set("1");
+				lui.set("0");
+				break;
+
+			case "100000":
+				regDst.set("0");
+				branch.set("0");
+				memRead.set("1");
+				memToReg.set("1");
+				memWrite.set("0");
+				ALUSrc.set("1");
+				regWrite.set("1");
+				byteOp.set("1");
+				memToMem.set("0");
+				signed.set("1");
+				ALUOpOut.set("000");
+				jump.set("0");
+				link.set("0");
+				equal.set("1");
+				lui.set("0");
+				halfOp.set("0");
+				break;
+
+			case "100100":
+				regDst.set("0");
+				branch.set("0");
+				memRead.set("1");
+				memToReg.set("1");
+				memWrite.set("0");
+				ALUSrc.set("1");
+				regWrite.set("1");
+				byteOp.set("1");
+				memToMem.set("0");
+				signed.set("0");
+				ALUOpOut.set("000");
+				jump.set("0");
+				link.set("0");
+				equal.set("1");
+				lui.set("0");
+				halfOp.set("0");
+				break;
+
+			case "101000":
+				branch.set("0");
+				memRead.set("1");
+				memWrite.set("1");
+				ALUSrc.set("1");
+				regWrite.set("0");
+				byteOp.set("1");
+				memToMem.set("1");
+				signed.set("1");
+				ALUOpOut.set("000");
+				jump.set("0");
+				equal.set("1");
+				lui.set("0");
+				halfOp.set("0");
+				break;
+
+			case "000100":
+				branch.set("1");
+				memWrite.set("0");
+				ALUSrc.set("0");
+				regWrite.set("0");
+				signed.set("1");
+				ALUOpOut.set("001");
+				jump.set("0");
+				equal.set("1");
+				lui.set("0");
+				break;
+
+			case "000010":
+				memWrite.set("0");
+				regWrite.set("0");
+				jump.set("1");
+				equal.set("1");
+				break;
+
+			case "000011":
+				memToReg.set("0");
+				memWrite.set("0");
+				regWrite.set("1");
+				memToMem.set("0");
+				jump.set("1");
+				link.set("1");
+				equal.set("1");
+				break;
+
+			case "001010":				//15
+				regDst.set("0");
+				branch.set("0");
+				memToReg.set("0");
+				memWrite.set("0");
+				ALUSrc.set("1");
+				regWrite.set("1");
+				memToMem.set("0");
+				signed.set("1");
+				ALUOpOut.set("011");
+				jump.set("0");
+				link.set("0");
+				equal.set("1");
+				lui.set("0");
+				break;
+
 		}
 	}
 }
